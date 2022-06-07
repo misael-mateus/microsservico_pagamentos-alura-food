be45f14a67de04e5250f76538e079c45ae55c495
misael mateus <misaelmateus.java@gmail.com>
2022-06-07 10:30:33 -0300

    Subindo mvc sem banco configurado

---
+++ src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -0,0 +1,55 @@
+package br.com.alurafood.pagamentos.service;
+
+import br.com.alurafood.pagamentos.dto.PagamentoDTO;
+import br.com.alurafood.pagamentos.model.Pagamento;
+import br.com.alurafood.pagamentos.model.Status;
+import br.com.alurafood.pagamentos.repository.PagamentoRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class PagamentoService {
+
+    @Autowired
+    private PagamentoRepository repository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Page<PagamentoDTO> obterTodos(Pageable paginacao) {
+        return repository
+                .findAll(paginacao)
+                .map(pagamento -> modelMapper.map(pagamento, PagamentoDTO.class));
+    }
+
+    public PagamentoDTO obterPorId(Long id) {
+        Pagamento pagamento = repository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+
+        return modelMapper.map(pagamento, PagamentoDTO.class);
+    }
+
+    public PagamentoDTO criarPagamento(PagamentoDTO dto) {
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setStatus(Status.CRIADO);
+        repository.save(pagamento);
+
+        return modelMapper.map(pagamento, PagamentoDTO.class);
+    }
+
+    public PagamentoDTO atualizarPagamento(Long id, PagamentoDTO dto) {
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setId(id);
+        pagamento = repository.save(pagamento);
+        return modelMapper.map(pagamento,PagamentoDTO.class);
+    }
+
+    public void excluirPagamento(Long id){
+        repository.deleteById(id);
+    }
+}
